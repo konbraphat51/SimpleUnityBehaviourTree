91f70c6057d6afb17e12d751b00cb175d1d1f250
konbraphat <konbraphat@gmail.com>
2025-10-31 04:46:01 +0900

    serialize by Monobehavior

--- Sample/SampleTree.cs
+++ Sample/SampleTree.cs
@@ -1,30 +1,33 @@
 using BehaviorTree.Nodes;
+using BehaviorTree.Serializations;
+using UnityEngine;
 
 namespace BehaviorTree.Sample
 {
-    public class SampleTreeRoot : BehaviorTree<int>
+    public class SampleTreeRoot : MonoBehaviour
     {
-        public SampleTreeRoot()
-            : base(
-                "SampleTreeRoot",
-                new Sequence<int>(
-                    new Node<int>[]
-                    {
-                        new Condition<int>(
-                            new And<int>(
-                                new ConditionEvaluator<int>[]
-                                {
-                                    new Not<int>(new SampleEvaluator<int>(10, 1.5f)),
-                                    new SampleEvaluator<int>(20, 2.5f),
-                                }
-                            ),
-                            new SampleAction<int>(1, 2.0f),
-                            new SampleAction<int>(2, 3.0f)
+        void Start()
+        {
+            Node<int> root = new Sequence<int>(
+                new Node<int>[]
+                {
+                    new Condition<int>(
+                        new And<int>(
+                            new ConditionEvaluator<int>[]
+                            {
+                                new Not<int>(new SampleEvaluator<int>(10, 1.5f)),
+                                new SampleEvaluator<int>(20, 2.5f),
+                            }
                         ),
-                        new SampleAction<int>(0, 0.0f),
-                    }
-                ),
-                1
-            ) { }
+                        new SampleAction<int>(1, 2.0f),
+                        new SampleAction<int>(2, 3.0f)
+                    ),
+                    new SampleAction<int>(0, 0.0f),
+                }
+            );
+
+            string serializedTree = Serializer<int>.WriteNodeJson(root);
+            Debug.Log(serializedTree);
+        }
     }
 }
